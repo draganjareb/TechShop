33f182368761566af470acc8b61f856fdd6e633f
draganjareb <dragan931@live.com>
2024-07-05 22:25:07 +0200

    add aplication on mobile offer

--- src/festures/Offer/components/mobile/Mobile.jsx
+++ src/festures/Offer/components/mobile/Mobile.jsx
@@ -1,19 +1,12 @@
 import OfferNavigation from "../offerNavigation/OfferNavigation";
 import "../mobile/mobile.style.css";
 import MobileNavigation from "./mobileNavigation/MobileNavigation";
-import { mobileData } from "../../../../data/mobileData/mobileData";
-import Item from "../../../../components/item/Item";
 
 const Mobile = () => {
   return (
     <div className="mobileWrapper">
       <OfferNavigation />
       <MobileNavigation />
-      <div className="mobileDataList">
-        {mobileData.map((item) => (
-          <Item key={item.key} item={item} />
-        ))}
-      </div>
     </div>
   );
 };
--- src/festures/Offer/components/mobile/mobileNavigation/MobileNavigation.jsx
+++ src/festures/Offer/components/mobile/mobileNavigation/MobileNavigation.jsx
@@ -5,40 +5,68 @@ import iPhoneLogo from "../../../../../assets/pictures/iPhoneLogo.png";
 import xiamioLogo from "../../../../../assets/pictures/xiamioLogo.png";
 import huaweiLogo from "../../../../../assets/pictures/huaweiLogo.png";
 import nokiaLogo from "../../../../../assets/pictures/nokiaLogo.png";
+import Item from "../../../../../components/item/Item";
+import { mobileData } from "../../../../../data/mobileData/mobileData";
+import { useState } from "react";
 
 const MobileNavigation = () => {
+  const [selectedGroup, setSelectedGroup] = useState("All");
+
+  const filteredMobile =
+    selectedGroup === "All"
+      ? mobileData
+      : mobileData.filter((work) => work.group === selectedGroup);
+
   return (
     <div className="mobileNavWrapper">
       <div className="mobileNavHolder">
-        <Link to={{ pathname: "/offer/mobile" }} className="allPhone">
+        <Link
+          to={{ pathname: "/offer/mobile" }}
+          className={selectedGroup === "All" ? "active" : ""}
+          onClick={() => setSelectedGroup("All")}
+        >
           SVI
         </Link>
 
         <img
           src={samsungLogo}
           alt="samsungLogo"
-          className="samsungLogoPictures"
+          className={selectedGroup === "SamsungPhone" ? "active" : ""}
+          onClick={() => setSelectedGroup("SamsungPhone")}
         />
 
         <img
           src={iPhoneLogo}
           alt="iPhoneLogo"
-          className="samsungLogoPictures"
+          className={selectedGroup === "IPhonePhone" ? "active" : ""}
+          onClick={() => setSelectedGroup("IPhonePhone")}
         />
 
         <img
           src={xiamioLogo}
           alt="xiamioLogo"
-          className="samsungLogoPictures"
+          className={selectedGroup === "XiamioPhone" ? "active" : ""}
+          onClick={() => setSelectedGroup("XiamioPhone")}
         />
 
         <img
           src={huaweiLogo}
           alt="huaweiLogo"
-          className="samsungLogoPictures"
+          className={selectedGroup === "HuaweiPhone" ? "active" : ""}
+          onClick={() => setSelectedGroup("HuaweiPhone")}
         />
 
-        <img src={nokiaLogo} alt="nokiaLogo" className="samsungLogoPictures" />
+        <img
+          src={nokiaLogo}
+          alt="nokiaLogo"
+          className={selectedGroup === "NokiaPhone" ? "active" : ""}
+          onClick={() => setSelectedGroup("NokiaPhone")}
+        />
+      </div>
+      <div className="mobileDataList">
+        {filteredMobile.map((item) => (
+          <Item key={item.key} item={item} />
+        ))}
       </div>
     </div>
   );
