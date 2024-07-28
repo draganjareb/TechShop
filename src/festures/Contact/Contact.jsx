8e0a8218aada16cd3a6c1abf058d0a3342272f42
draganjareb <dragan931@live.com>
2024-07-28 20:35:30 +0200

    finish about page

--- src/festures/Contact/Contact.jsx
+++ src/festures/Contact/Contact.jsx
@@ -1,7 +1,33 @@
 import "../Contact/contact.style.css";
+import maps from "../.."
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <div className="contactWrapper">
+      <div className="contactHolder">
+        <div className="contactLeftSite">
+          <h2>Možete nas kontaktirati na sledeće načine:</h2>
+          <div className="contactData">
+            <div>
+              <h3>Telefon:</h3>
+              <p>065/875-041</p>
+            </div>
+            <div>
+              <h3>e-mail:</h3>
+              <p>dragan931@live.com</p>
+            </div>
+          </div>
+          <div>
+            <h2>Imate pitanje za nas ili kritiku molimo upišite ovdje</h2>
+            <input placeholder="Text..." />
+          </div>
+        </div>
+        <div className="contactRightSite">
+          <img src={} alt="map"
+                  </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- src/festures/Partners/Partners.jsx
+++ src/festures/Partners/Partners.jsx
@@ -1,7 +1,81 @@
 import "../Partners/partners.style.css";
+import fedex from "../../assets/pictures/fedex.jpg";
+import meeting from "../../assets/pictures/meeting.png";
+import euroexpress from "../../assets/pictures/euroexpress.png";
+import a2b from "../../assets/pictures/a2b.jpg";
 
 const Partners = () => {
-  return <div>Partners</div>;
+  return (
+    <div className="partnersWrapper">
+      <div className="partnersHolder">
+        <div className="partnersCard">
+          <img src={meeting} alt="meeting" className="partnersFisrtImg" />
+          <h2>
+            U današnjem vremenu kada je konkurencija velika važno je naći dobre
+            partnere za saradnju. Pored tržišta koje je jedno od osnovnih uslova
+            za dobro poslovanje i dobar uspjeh firme treba istaknuti partnere sa
+            kojima imamo važnu, značajnu saradnju da naši proizvodi brzo i
+            kvalitetno dođu do naših kupaca.
+          </h2>
+        </div>
+        <div className="partnersCard">
+          <img src={fedex} alt="FedEx" />
+          <div>
+            <p>
+              FedEx Express je glavna američka kargo aviokompanija sa sjedištem
+              u Memphisu, Tennessee, Sjedinjene Američke Države. Od 2023.
+              godine, to je najveća svjetska kargo aviokompanija u smislu
+              veličine flote i tona tereta koji se preveze. To je istoimena i
+              vodeća podružnica korporacije FedEx, koja svakodnevno isporučuje
+              teret i pakete na više od 375 destinacija u 220 zemalja na šest
+              kontinenata. FedEx Express je također najveća svjetska kompanija
+              za ekspresni transport.
+            </p>
+            <a
+              href="https://en.wikipedia.org/wiki/FedEx_Express"
+              target="_blank"
+            >
+              Pročitaj još...
+            </a>
+          </div>
+        </div>
+        <div className="partnersCard">
+          <div>
+            <p>
+              Osnovani smo 2008. godine sa sjedištem u Banjaluci, te smo jasnom
+              strategijom, stručnim i kvalitetnim kadrom i povjerenjem korisnika
+              razvili mrežu svojih poslovnica sa kojom obezbjeđujemo 100%
+              pokrivenosti svih mjesta i gradova u zemlji.
+            </p>
+            <a
+              href="https://www.euroexpress.ba/o-nama/istorijat"
+              target="_blank"
+            >
+              Pročitaj još...
+            </a>
+          </div>
+          <img src={euroexpress} alt="euroexpress" />
+        </div>
+        <div className="partnersCard">
+          <img src={a2b} alt="a2b" />
+          <div>
+            <p>
+              A2B je najveća domaća transportna kompanija koja nudi raznovrstan
+              portfolio različitih usluga na svim nivoima. Svojim kapacitetima
+              nudi klijentima najsavremeniju i najpouzdaniju transportnu mrežu
+              za slanje i prijem pošiljki. Naša vodilja je potpuna orjentacija
+              ka Vašim potrebama u ovom polju, kako bi se Vi što uspješnije
+              takmičili na domaćem tržištu. Saradnja sa A2B čini poslovanje u
+              zemlji jednostavnijim, lakšim i profitabilnijim.
+            </p>
+            <a href="https://a2b.ba/o-nama/" target="_blank">
+              Pročitaj još...
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Partners;
